77612fbc6c7947672477ad5497737ea2cf648e1f
Geoffrey Crofte <crg@foyer.lu>
2019-09-16 17:50:28 +0200

    Homepage first shot

--- src/js/main.js
+++ src/js/main.js
@@ -32,31 +32,3 @@
 	searchShadow.addEventListener('click', closeMenu);
 
 })();
-// Production steps of ECMA-262, Edition 5, 15.4.4.18
-// Reference: http://es5.github.io/#x15.4.4.18
-if (!Array.prototype.forEach) {
-
-    Array.prototype.forEach = function (callback/*, thisArg*/) {
-        var T, k;
-        if (this == null) {
-            throw new TypeError('this is null or not defined');
-        }
-        var O = Object(this);
-        var len = O.length >>> 0;
-        if (typeof callback !== 'function') {
-            throw new TypeError(callback + ' is not a function');
-        }
-        if (arguments.length > 1) {
-            T = arguments[1];
-        }
-        k = 0;
-        while (k < len) {
-            var kValue;
-            if (k in O) {
-                kValue = O[k];
-                callback.call(T, kValue, k, O);
-            }
-            k++;
-        }
-    };
-}
