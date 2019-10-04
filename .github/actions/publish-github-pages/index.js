7f2e339a5813d20ce175f13ec47607e37a0e3ae9
Bastien Collet <bastien.collet1@gmail.com>
2019-10-04 15:46:38 +0200

    Fix an issue with the gulpfile path

--- .github/actions/publish-github-pages/index.js
+++ .github/actions/publish-github-pages/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const execSync = require('child_process').execSync;
 
 try {
-    if (fs.existsSync(gulpfile.js)) {
+    if (fs.existsSync('gulpfile.js')) {
         execSync('gulp publish-documentation');
     }
 } catch(err) {
