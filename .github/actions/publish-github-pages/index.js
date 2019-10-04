68b7d0cb92879d23f98ca63aeefea3c557f19545
Bastien Collet <bastien.collet1@gmail.com>
2019-10-04 15:48:49 +0200

    Add the npm i step to the github action

--- .github/actions/publish-github-pages/index.js
+++ .github/actions/publish-github-pages/index.js
@@ -3,6 +3,7 @@ const execSync = require('child_process').execSync;
 
 try {
     if (fs.existsSync('gulpfile.js')) {
+        execSync('npm i');
         execSync('gulp publish-documentation');
     }
 } catch(err) {
