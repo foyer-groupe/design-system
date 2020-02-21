915ff7a9ee767189bb32cff529d22391c147fec7
geoffreycrofte <geoffreycrofte@users.noreply.github.com>
2020-02-21 15:13:57 +0000

    Automated deployment: Fri Feb 21 15:13:57 UTC 2020 b3f3e14f23547c197c0c311995a98b586e03dd76

--- sw-register.js
+++ sw-register.js
@@ -1,9 +1,13 @@
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', function() {
-		navigator.serviceWorker.register('/sw.js').then(function(registration) {
-			console.info('[SW] Registration successful with scope: ' + registration.scope);
-		}, function(err) {
-			console.info('[SW] Registration failed: ' + err);
-		});
+		navigator.serviceWorker
+			.register('/sw.js', {
+				scope: './'
+			})
+			.then(function(registration) {
+				console.info('[SW] Registration successful with scope: ' + registration.scope);
+			}, function(err) {
+				console.info('[SW] Registration failed: ' + err);
+			});
 	});
 }
