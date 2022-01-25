98c6bddb74f7e610841968f5372b7ef3bc4ddeb6
Geoffrey Crofte <support@creativejuiz.com>
2022-01-25 11:56:54 +0100

    Update de gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 // ------------------------------------------------------------------------------------------------------------------ //
 //          DEV TASKS MANAGER
 // ------------------------------------------------------------------------------------------------------------------ //
+
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const csso = require('gulp-csso');
 const concat = require('gulp-concat');
@@ -37,7 +38,7 @@ gulp.task('default', function (done) {
 
 			// Compile CSS files
 			gulp.src('src/css/main.scss')
-				.pipe(sass())
+				.pipe(sass.sync().on('error', sass.logError))
 				.pipe(autoprefixer())
 				.pipe(csso())
 				.pipe(gulp.dest('public/css/')),
